7cab5522effd2c23c8f6f9dca9e18dd767a4ea75
Miroslav Ivanov <znaka5@abv.bg>
2023-12-30 18:06:16 +0200

    mergeSort

---
+++ Sorting Algorithms/mergeSort.js
@@ -0,0 +1,38 @@
+function mergeSort(arr) {
+    if (arr.length === 1) return arr;
+    const mid = Math.floor(arr.length / 2);
+    const left = mergeSort(arr.slice(0, mid));
+    const right = mergeSort(arr.slice(mid));
+    return merge(left, right)
+
+}
+
+function merge(leftPart, rightPart) {
+    let i = 0;
+    let j = 0;
+    const mergedArr = [];
+    while (leftPart.length > i && rightPart.length > j) {
+        if (leftPart[i] > rightPart[j]) {
+            mergedArr.push(rightPart[j]);
+            j++;
+        } else {
+            mergedArr.push(leftPart[i]);
+            i++;
+        }
+    }
+
+    while (i <= leftPart.length - 1) {
+        mergedArr.push(leftPart[i]);
+        i++;
+    }
+
+    while (j <= rightPart.length - 1) {
+        mergedArr.push(rightPart[j]);
+        j++
+    }
+
+    return mergedArr;
+
+}
+
+console.log(mergeSort([34,2,1,3]));
