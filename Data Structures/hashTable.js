b50c573b71466df03c05170ca0da88859fa0c07e
Miroslav Ivanov <znaka5@abv.bg>
2024-01-10 23:54:17 +0200

    HashTable Implementation

---
+++ Data Structures/hashTable.js
@@ -0,0 +1,25 @@
+class HashTable {
+    constructor(length) {
+        this.values = new Array(length)
+    }
+
+    hash() {
+
+    }
+
+    set(params) {
+
+    }
+
+    get() {
+
+    }
+
+    keys() {
+
+    }
+
+    values() {
+
+    }
+}
